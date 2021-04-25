18a88cb5749a4fe719e97b68131c851168caaf40
alexp799 <popovperm159@gmail.com>
2021-04-25 14:25:57 +0300

    24apr13

--- app/fillthedata.py
+++ app/fillthedata.py
@@ -24,7 +24,7 @@ def fill_db(pathtoall):
             nname = os.path.basename(file)
 
             index = nname.index('.')
-            nname = nname[13:index]
+            nname = nname[17:index]
             obj = SModel(name=str(nname), path=str(imname))
             print(obj)
             db.session.add(obj)
@@ -36,7 +36,7 @@ def fill_db(pathtoall):
 
            sroute = stat
            l = len(sroute)
-           sroute = sroute[13:l - 13]
+           sroute = sroute[17:l - 13]
            print(sroute)
            sl=len(stat)
            sname=stat[sl-12:sl-4]
